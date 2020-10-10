0d3b7a60cd3408299e8e5f6de66521cee9313454
Jeremy Doubleday <phate6983@gmail.com>
2020-10-10 16:11:23 -0500

    updating colors and left nav bar

--- lendingservice-frontend/src/MainNav.tsx
+++ lendingservice-frontend/src/MainNav.tsx
@@ -3,9 +3,28 @@ import React from 'react';
 function MainNav() {
     return(
       <ul className="Nav">
-        <li>Nav Item 1</li>
-        <li>Nav Item 2</li>
-        <li>Nav Item 3</li>
+        <li className="leftNav"><p>Distance Selector:</p><p>5 miles to 50 miles</p>
+          <input type="range" id="rangeSelector" list="mileRange" min="5" max="50" step="5" value="5"/>
+          <datalist id="mileRange">
+            <option value="5" />
+            <option value="10" />
+            <option value="15" />
+            <option value="20" />
+            <option value="25" />
+            <option value="30" />
+            <option value="35" />
+            <option value="40" />
+            <option value="45" />
+            <option value="50" />
+          </datalist>
+        </li>
+        <li className="leftNav">
+          <select id="equipmentSelector">
+            <option value="wifi">Wifi</option>
+            <option value="brandon">Brandon</option>
+          </select>
+        </li>
+        <li className="leftNav">Nav Item 3</li>
       </ul>
     )
   }
