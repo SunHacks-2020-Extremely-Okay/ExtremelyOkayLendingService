14cd5d7e2e0f2cffeafb78ad6aaf61e5a2b4e58a
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 15:55:51 -0700

    Format items reading in

--- lendingservice-frontend/src/ItemBrowse.tsx
+++ lendingservice-frontend/src/ItemBrowse.tsx
@@ -6,8 +6,18 @@ function ItemBrowse() {
     return (
       <div className="BrowseItems">
         <ul>
-          {itemsArray.map((value:any, index:any) => {
-            return <li key={index}><div className="Item-location"><p>{value.user_id}<br />Item ID={value.item_id}<br />Latitude={value.location_x}<br />Latitude={value.location_y}</p></div><div className="Item-interact"><button>Reserve</button></div></li>
+          {itemsArray.map((value: any, index: any) => {
+            return <li key={index}>
+              <div className="Item-location">
+                <p>{value.user_id}<br />
+                Item ID={value.item_id}<br />
+                Latitude={value.location_x}<br />
+                Latitude={value.location_y}</p>
+              </div>
+              <div className="Item-interact">
+                <button>Reserve</button>
+              </div>
+            </li>
           })}
         </ul>
       </div>
