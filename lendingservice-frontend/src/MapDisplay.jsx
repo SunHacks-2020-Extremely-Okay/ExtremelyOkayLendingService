81b3de443a5350284edcb4dff5b2cbc8ce6b944f
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 12:06:26 -0700

    Rename file and get map loading

---
+++ lendingservice-frontend/src/MapDisplay.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import GoogleMapReact from 'google-map-react';
+
+class MapDisplay extends Component {
+  static defaultProps = {
+    center: {
+      lat: 33.4226584,
+      lng: -111.9401533
+    },
+    zoom: 13
+  }
+
+  render() {
+    return (
+      <div className="Map-display">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDEMqN5k3XDbLI5RkHpwHd3AJurzsfiiw8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default MapDisplay;
