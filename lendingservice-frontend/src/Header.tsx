20ce8fa1d0d1bb76ae2572bb48d1469221c204bc
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 19:42:18 -0700

    Add modal to hold our form to add devices

--- lendingservice-frontend/src/Header.tsx
+++ lendingservice-frontend/src/Header.tsx
@@ -1,15 +1,58 @@
 import React from 'react';
+import Modal from 'react-modal';
+
+const customModalStyles = {
+    content : {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+}
+
+
+Modal.setAppElement('#root');
 
 function Header() {
+    const [modalIsOpen,setIsOpen] = React.useState(false);
+
+    function openAddModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+
+    }
+
+    function closeAddModal() {
+        setIsOpen(false);
+    }
+
     return (
-      <header className="Header">
+        <header className="Header">
         <div className="Header-brand"><p>Extremely Okay Lending service</p></div>
         <div className="Header-nav">
-          <div className="tab"><a href="#">Add</a></div>
+          <div className="tab" onClick={openAddModal}><a href="#">Add</a></div>
           <div className="tab"><a href="#">Browse</a></div> 
           <div className="tab"><a href="#">Account</a></div>
         </div>
-      </header>
+      
+
+      <Modal 
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeAddModal}
+        style={customModalStyles}
+        contentLabel="Add Device To Database"
+        >
+            <button onClick={closeAddModal}>Close</button>
+            <form>
+                <input />
+            </form>
+        </Modal>
+        </header>
     )
   }
 
--- lendingservice-frontend/src/MainNav.tsx
+++ lendingservice-frontend/src/MainNav.tsx
@@ -29,9 +29,9 @@ function MainNav() {
         <li className="leftNav">      
           <SignUpForm />
         </li>
-
-
       </ul>
+
+
     )
   }
 
