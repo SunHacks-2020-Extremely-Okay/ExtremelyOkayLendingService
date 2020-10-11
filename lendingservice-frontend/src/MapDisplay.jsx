b7c6e7224ecee6cb1d7b73865f43a3715121ce28
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 17:16:13 -0700

    Add InfoWindow display

--- lendingservice-frontend/src/MapDisplay.jsx
+++ lendingservice-frontend/src/MapDisplay.jsx
@@ -4,31 +4,51 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 let itemsArray = require('./items.js');
 
 export class MapDisplay extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
   render() {
     var bounds = new this.props.google.maps.LatLngBounds();
     var points = new Array();
-    for(let i = 0; i < itemsArray.length; i++) {
-      points.push(itemsArray[i].location_x, itemsArray[i].location_y);
-      //bounds.extend(points[i]);
+    for (let i = 0; i < itemsArray.length; i++) {
+      points.push({ lat: itemsArray[i].location_x, lng: itemsArray[i].location_y });
+      bounds.extend(points[i]);
     }
 
     return (
       <div className="Map-display">
-        <Map 
-          google={this.props.google} 
+        <Map
+          google={this.props.google}
+          bounds={bounds}
           zoom={13}
           initialCenter={{
             lat: 33.4226584,
             lng: -111.9401533
-          }}
-          bounds={bounds}>
-            {/* Loop over itemsArray and plot all the points */}
-            {itemsArray.map((value, index) => {
-              return <Marker 
-                name={value.user_id}
-                position={{lat:value.location_x, lng:value.location_y}} />
-            })}
+          }}>
+          {/* Loop over itemsArray and plot all the points */}
+          {itemsArray.map((value, index) => {
+            return <Marker
+              name={value.user_id}
+              position={{ lat: value.location_x, lng: value.location_y }}
+              onClick={this.onMarkerClick} />
+          })}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
         </Map>
       </div>
     );
