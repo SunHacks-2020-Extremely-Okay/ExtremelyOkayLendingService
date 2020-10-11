d0c81f415001f256a5c0efa4408cd34c2b674422
json <jtmichel@asu.edu>
2020-10-10 18:12:57 -0600

    Create intial APIs, hook them up to ES adapter, and add a document mapper.

---
+++ LendingService-Backend/src/main/java/com/ExtremelyOkayLending/models/Greeting.java
@@ -0,0 +1,11 @@
+package com.ExtremelyOkayLending.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Greeting {
+    private final long id;
+    private final String content;
+}
--- LendingService-Backend/src/main/java/com/ExtremelyOkayLending/service/GreetingController.java
+++ LendingService-Backend/src/main/java/com/ExtremelyOkayLending/service/GreetingController.java
@@ -1,10 +1,12 @@
 package com.ExtremelyOkayLending.service;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import example.models.Greeting;
 
 @RestController
 public class GreetingController {
