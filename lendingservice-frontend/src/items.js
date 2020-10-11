eaf2ecb8e9e46c69f2813440f35753cb0b014248
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 17:33:23 -0700

    Add description property

--- lendingservice-frontend/src/items.js
+++ lendingservice-frontend/src/items.js
@@ -2,6 +2,7 @@ var itemsArray = [
     {
         "item_id": 5423452345,
         "user_id": "bllarkin",
+        "description": "Macbook Pro",
         "category": 5,
         "location_x": 33.459679,
         "location_y": -112.0549142,
@@ -13,6 +14,7 @@ var itemsArray = [
     {
         "item_id": 34573456,
         "user_id": "jwdouble",
+        "description": "Dell XPS",
         "category": 5,
         "location_x": 60.3880863,
         "location_y": -111.8722263,
