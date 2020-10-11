f9e17ba18b910a9344074ae754094b6efdf19c12
Brandon Larkin <bllarkin@gmail.com>
2020-10-10 21:50:16 -0700

    Populate meaningful test values

--- lendingservice-frontend/src/Equipment.tsx
+++ lendingservice-frontend/src/Equipment.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Equipment() {
     return (
         <label>Type:
-            <select id="equipmentSelector">
+            <select id="description">
                 <option value="wifi">Wifi</option>
-                <option value="brandon">Brandon</option>
+                <option value="winlaptop">Windows Laptop</option>
+                <option value="maclaptop">Mac Laptop</option>
+                <option value="rpi">Raspberry Pi</option>
             </select>
         </label>
     )
